b85c77cfd007fbf0303fc338c107f9b9ad7b2dcf
MherHayrapetyan <hayrapetyanmher64@gmail.com>
2023-03-23 17:24:23 +0400

    C++ Page + Media

---
+++ CPlPl/cPlPl.js
@@ -0,0 +1,48 @@
+// Header Burger
+
+const burgerBigBox = document.querySelector(".burgerBigBox");
+let i = 0;
+function burgerMenu() {
+  i++;
+  burgerBigBox.style.animationIterationCount = i;
+}
+
+// Header Burger Close
+
+// Akardion + Video
+
+const akardionBigBox = document.querySelector(".htmlSovBigBoxInformVideo")
+
+class NewVideoDiv {
+  constructor(num, src) {
+    this.$div = document.createElement("div");
+    this.$div.innerHTML = `<div class="htmlSovBigBoxInformVideoOne">C++ - ${num} Վիդեոդաս</div>
+    <div class="htmlSovBigBoxInformVideoOneVideo">
+      <iframe
+        width="100%"
+        height="100%"
+        src=${src}
+      >
+      </iframe>
+    </div>`;
+    akardionBigBox.appendChild(this.$div)
+  }
+}
+
+for (i = 1; i <= 12; i++) {
+  new NewVideoDiv(i, `https://www.youtube.com/watch?v=hi38vVvJvFE&list=PLkVo56yGU5PpBU1YjOlNzGH5xGgGaTGg3&index=${i}`)
+  console.log(i)
+}
+
+const videoText = document.querySelectorAll(".htmlSovBigBoxInformVideoOne");
+const videoYoutub = document.querySelectorAll(
+  ".htmlSovBigBoxInformVideoOneVideo"
+);
+
+videoText.forEach((item, index) => {
+  item.onclick = () => {
+    videoYoutub[index].classList.toggle("height");
+  };
+});
+
+// Akardion + Video Close
